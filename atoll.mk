e8f432a87a0a7c80fd1ad56e94a38b6669574fa5
Leonardo Ledda <leonardoledda@gmail.com>
2023-09-30 19:10:32 +0200

    sm6250-common: Reorganize overlays
    * Move Aperture and Dialer overlays to main directory
    * Delete Lineage SDK overlay
    
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -251,15 +251,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    SM6250Aperture \
+    SM6250Dialer \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
     WifiOverlay
-
-PRODUCT_PACKAGES += \
-    SM6250Aperture \
-    SM6250LineageDialer \
-    SM6250LineageSDK
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
